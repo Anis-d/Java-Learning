ff04b85de113a32d2e5df787c1a0565d120fa5fc
Anish kumar <101183988+Anis-d@users.noreply.github.com>
2022-10-10 22:27:18 +0530

    Add files via upload

---
+++ sortarray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class sortarray {
+
+	public static void main(String[] args) {
+		String s="Aneesh kumar";
+		char ch[]=s.toCharArray();
+		Arrays.sort(ch);
+		System.out.println(new String(ch));
+
+	}
+
+}
